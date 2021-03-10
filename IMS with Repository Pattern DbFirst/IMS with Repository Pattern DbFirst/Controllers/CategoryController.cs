f87a3ef690638910c40af4f4e33acc94bfa98f8d
m9993 <muntasiralam9993@gmail.com>
2021-03-10 16:41:45 +0600

    added

---
+++ IMS with Repository Pattern DbFirst/IMS with Repository Pattern DbFirst/Controllers/CategoryController.cs
@@ -0,0 +1,61 @@
+﻿using IMS_with_Repository_Pattern_DbFirst.Models;
+using IMS_with_Repository_Pattern_DbFirst.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS_with_Repository_Pattern_DbFirst.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryRepository categoryRepository = new CategoryRepository();
+        public ActionResult Index()
+        {
+            return View(categoryRepository.GetAll());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryRepository.Insert(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return View(categoryRepository.Get(id));
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(categoryRepository.Get(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Category category)
+        {
+            var categoryToEdit = categoryRepository.Get(id);
+            categoryToEdit.CategoryName = category.CategoryName;
+            categoryRepository.Update(categoryToEdit);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View(categoryRepository.Get(id));
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            categoryRepository.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
